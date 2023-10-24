813d0f91dc821a7bc0528b015b785bcd7e31c454
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-24 12:08:22 +0200

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
+const staticCacheName = 'stite-static-v1.6'
 
-// Install Service Worker
-self.addEventListener('install', event => {
+const assets = [
+    "/",
+    "/index.html",
+    "/css/styles.css"
+]
+
+
+// Install event cache
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('write asset file to cache');
+            cache.addAll(assets)
+        })
+    )
+
+
 	console.log('Service Worker has been installed');
 })
 
@@ -12,4 +28,25 @@ self.addEventListener('activate', event => {
 // Fetch event
 self.addEventListener('fetch', event => {
 	console.log('Fetch event is here!', event)
+})
+
+//activate event
+self.addEventListener('activate', (event) => {
+	console.log(' activate event is here!', event);
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+            const filteredkeys = keys.filter(key => key !== staticCacheName )
+            // console.log(filteredkeys);
+            filteredkeys.map(key => {
+                caches.delete(key)
+            })
+
+
+            // keys.filter(key => key !== staticCacheName).map
+        })
+    )
+    // console.log(caches.keys)
+
+
 })
