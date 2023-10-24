99c3ee79c3b659b7000bd276f238aeb8b703bb05
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-24 09:27:23 +0200

    service worker added - working

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+// Install Service Worker
+self.addEventListener('install', event => {
+	console.log('Service Worker has been installed');
+})
+
+// Install Service Worker
+self.addEventListener('activate', event => {
+	console.log('Service Worker has been activated');
+})
+
+// Fetch event
+self.addEventListener('fetch', event => {
+	console.log('Fetch event is here!', event)
+})
