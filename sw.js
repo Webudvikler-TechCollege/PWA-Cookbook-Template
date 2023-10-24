4479ccc1714d045196f6274c49dd3d2c4b46e395
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-24 14:08:47 +0200

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const staticCacheName = 'stite-static-v1.6'
+//Navn og vision på cache samling 
+const staticCacheName = 'stite-static-v1.4'
+
+const dynamicCacheName = 'site-dynamic-v1'
 
 const assets = [
     "/",
@@ -50,3 +53,37 @@ self.addEventListener('activate', (event) => {
 
 
 })
+//forskell på parantesser ved event (check it)
+self.addEventListener('fetch',  event => {
+    // console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return (cacheRes)
+        })
+
+    )
+
+
+})
+
+// Fetch event
+self.addEventListener('fetch', event => {
+	// Kontroller svaret på vores request
+	event.respondWith(
+		// Kig efter file match i cache 
+		caches.match(event.request).then(cacheRes => {
+			// Returner match fra cache - ellers hent fil på server
+			return cacheRes || fetch(event.request).then(async fetchRes => {
+				// Tilføjer nye sider til cachen
+				return caches.open(dynamicCacheName).then(cache => {
+					// Bruger put til at tilføje sider til vores cache
+					// Læg mærke til metoden clone
+					cache.put(event.request.url, fetchRes.clone())
+					// Returnerer fetch request
+					return fetchRes
+				})
+			})
+		})
+	)
+})
+
