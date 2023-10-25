9ed07f761f1001516f1105bc33863b68c0329d32
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-25 13:19:14 +0200

    fallback update

--- sw.js
+++ sw.js
@@ -6,127 +6,84 @@ const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
     "/",
     "/index.html",
-    "/css/styles.css"
+    "/css/styles.css",
+	"/fallback.html"
+
 ]
 
-
-// Install event cache
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            console.log('write asset file to cache');
-            cache.addAll(assets)
-        })
-    )
-
-
-	console.log('Service Worker has been installed');
-})
-
-// Install Service Worker
-self.addEventListener('activate', event => {
-	console.log('Service Worker has been activated');
-})
-
-// Fetch event
-self.addEventListener('fetch', event => {
-	console.log('Fetch event is here!', event)
-})
-
-//activate event
-self.addEventListener('activate', (event) => {
-	console.log(' activate event is here!', event);
-
-    event.waitUntil(
-        caches.keys().then(keys => {
-            const filteredkeys = keys.filter(key => key !== staticCacheName )
-            // console.log(filteredkeys);
-            filteredkeys.map(key => {
-                caches.delete(key)
-            })
-
-
-            // keys.filter(key => key !== staticCacheName).map
-        })
-    )
-    // console.log(caches.keys)
-
-
-})
-//forskell på parantesser ved event (check it)
-self.addEventListener('fetch',  event => {
-    // console.log(event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(cacheRes => {
-            return (cacheRes)
-        })
-
-    )
-
-
-})
-
-// Fetch event
-self.addEventListener('fetch', event => {
-
-    if (!(event.request.url.indexOf('http') == 0)) return
-	// Kontroller svaret på vores request
+caches.open("my-cache").then((cache) => {
+	cache.addAll(assets);
+  });
+  
+  // Install Service Worker
+  
+  // Install event
+  self.addEventListener("install", (event) => {
+	console.log("Service Worker has been installed");
+  
+	event.waitUntil(
+	  caches.open(staticCacheName).then((cache) => {
+		console.log("Write asset files to cache");
+		cache.addAll(assets).catch((error) => {
+		  console.log(error, "Der er en fejl");
+		});
+	  })
+	);
+  });
+  
+  // Activate event
+  self.addEventListener("activate", (event) => {
+	console.log("Service Worker has been activated");
+	event.waitUntil(
+	  caches.keys().then((keys) => {
+		return Promise.all(
+		  keys
+  
+			.filter((key) => key !== staticCacheName)
+  
+			.map((key) => caches.delete(key))
+		);
+	  })
+	);
+  });
+  
+  // Fetch event here
+  self.addEventListener("fetch", (event) => {
+	if (!(event.request.url.indexOf("http") === 0)) return;
+  
 	event.respondWith(
-		// Kig efter file match i cache 
-		caches.match(event.request).then(cacheRes => {
-			// Returner match fra cache - ellers hent fil på server
-			return cacheRes || fetch(event.request).then(async fetchRes => {
-				// Tilføjer nye sider til cachen
-				return caches.open(dynamicCacheName).then(cache => {
-					// Bruger put til at tilføje sider til vores cache
-					// Læg mærke til metoden clone
-					cache.put(event.request.url, fetchRes.clone())
-					// Returnerer fetch request
-					return fetchRes
-				})
-			})
-		})
-	)
-})
-
-// Funktion til styring af antal filer i den given cache
-const limitCacheSize = (cacheName, numberOfAllowedFiles) => {
-	// Vi åbner den angivede cache
-	caches.open(cacheName).then(cache => {
-		// Henter array af cache keys 
-		cache.keys().then(keys => {
-			// Hvis mængden af filer overstiger det tilladte
-			if(keys.length > numberOfAllowedFiles) {
-				// Så slettes første index (ældste fil) og kør funktion igen indtil antal er nået
-				cache.delete(keys[0]).then(limitCacheSize(cacheName, numberOfAllowedFiles))
+	  caches.match(event.request).then((cacheRes) => {
+		return (
+		  cacheRes ||
+		  fetch(event.request).then(async (fetchRes) => {
+			if (fetchRes.status === 404) {
+			  console.log("Serving fallback page for 404 error");
+			  return caches.match("/fallback.html");
 			}
-		})
-	})
-}
+  
+			return caches.open(dynamicCacheName).then((cache) => {
+			  cache.put(event.request.url, fetchRes.clone());
+			  return fetchRes;
+			});
+		  })
+		);
+	  })
+	);
+  });
+  
+  
 
-// Calling limit cache function
-limitCacheSize(dynamicCacheName, 2)
-
-// Fetch event
-self.addEventListener('fetch', event => {
-
-	// Kontroller svar på request
-	event.respondWith(
-		
-		// Kig efter file match i cache 
-		caches.match(event.request).then(cacheRes => {
-			// Returner match fra cache / hent fil på server
-			// ...
-		}).catch(() => {
-			// Hvis ovenstående giver fejl kaldes fallback siden			
-			return caches.match('/pages/fallback.html')
-		})
-	)
-})
-
-// ...
-.catch(() => {
-	if(event.request.url.indexOf('.html') > -1) {
-		return caches.match('/pages/fallback.html')
-	}
-})
+	// Limit
+	const limitCacheTwo = (cacheName, numberOfAllowedFiles) => {
+	  caches.open(cacheName).then((cache) => {
+		cache.keys().then((keys) => {
+		  if (keys.length > numberOfAllowedFiles) {
+			cache
+			  .delete(keys[0])
+			  .then(limitCacheTwo(caches, numberOfAllowedFiles));
+		  }
+		});
+	  });
+	};
+  
+	limitCacheTwo(dynamicCacheName, 2);
